a2ae7d926e81afef61a9eb11bfe71b7f683a731c
kinseyost <kinsey.vanost@gmail.com>
2018-08-28 21:49:12 -0700

    Removes debugger

--- client/src/reducers/dateReducer.js
+++ client/src/reducers/dateReducer.js
@@ -5,7 +5,6 @@ export default function dateReducer(state = Immutable.Map(), action) {
     const { type, date } = action;
     switch (type) {
         case DATE_UPDATE:
-            debugger;
             return date;
         default:
             return state;
